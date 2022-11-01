cf70545ba47e5fa1a7f7b0aefb99fa2956b5c9b7
adedamolabasit <53799680+adedamolabasit@users.noreply.github.com>
2022-11-01 23:46:44 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  
     <div className='body w-screen  bg-[#E5E5E5] w-screen'>
     <App />
     </div>
-  </React.StrictMode>
+
 );
 
 
